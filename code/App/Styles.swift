a9cdefcd497011c2aefe8cedabb8c185a09453fe
Stephen Bechtold <sbechtold@buildinmotion.com>
2017-11-14 15:47:45 -0500

    Added files

---
+++ code/App/Styles.swift
@@ -0,0 +1,43 @@
+//
+//  Styles.swift
+//  Wrapper
+//
+//  Created by Stephen Bechtold on 12/21/16.
+//  Copyright © 2016 Build in Motion. All rights reserved.
+//
+
+import Foundation
+import UIKit;
+
+public class Styles {
+    
+    public class func apply() {
+        
+    }
+    
+}
+
+
+public class UIWrapperLoginView : UIView {
+    
+}
+
+public class UIWrapperLoginButton : UIButton {
+    
+}
+
+public class UIWrapperLoginTextField : UITextField {
+
+}
+
+public class UIWrapperLoginErrorLabel : UILabel {
+    
+}
+
+public class UIWrapperLoginLabel : UILabel {
+    
+}
+
+public class UIWrapperLoginSwitch : UISwitch {
+    
+}
